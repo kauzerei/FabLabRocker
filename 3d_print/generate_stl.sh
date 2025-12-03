6ca6e73008dd935bcb73728ec6bae2941211f28c
kauzerei <github@kauzerei.de>
2025-12-03 17:15:50 +0100

    separated from exposer repo

---
+++ 3d_print/generate_stl.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Script that generates 3d-printable .stl files from parametric .SCAD
+# models
+#
+# Copyright (c) 2023 - 2024 Kauzerei <mailto:github@kauzerei.de>
+# Copyright (c) 2023 - 2024 Thomas Buck <thomas@xythobuz.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+shopt -s expand_aliases
+
+command -v openscad >/dev/null 2>&1 || { alias openscad='openscad-nightly'; }
+command -v openscad >/dev/null 2>&1 || { echo >&2 "Openscad not installed. Abotring "; exit 1; }
+command -v pgrep >/dev/null 2>&1 || { echo >&2 "procps not installed. Aborting "; exit 1; }
+
+N=$(nproc --all)
+
+mkdir -p stl
+
+if [ -z "$( ls -A $(git rev-parse --show-toplevel)/3d_print/BOSL2 )" ]; then 
+echo "BOSL2 not found" && git submodule update --init
+fi
+
+for MODULE in $@
+do
+  MODULENAME=$(basename "$MODULE" ".${MODULE##*.}")
+  PARTS=$(grep -o "part.*//.*\[.*]" ${MODULE} | sed 's/,/ /g' | sed 's/.*\[\([^]]*\)\].*/\1/g')
+  echo "generating from ${MODULE}:"
+  if [ -z "$PARTS" ]; then 
+    openscad --q --enable roof --backend Manifold "$(cd "$(dirname "${MODULE}")" && pwd)/$(basename "${MODULE}")" --D part=\"${PART}\" --o $(pwd)/stl/${MODULENAME}.stl &
+    while [ $(pgrep -c openscad) -ge $N ]; do sleep 1; done
+  fi
+  for PART in ${PARTS}
+  do
+    if [[ "${PART}" != "NOSTL"* ]]; then
+      echo ${PART}
+      FILENAME=$(echo stl/${MODULENAME}_${PART}.stl | tr '[:upper:]' '[:lower:]')
+      openscad --q --enable roof --backend Manifold "$(cd "$(dirname "${MODULE}")" && pwd)/$(basename "${MODULE}")" --D part=\"${PART}\" --o $(pwd)/${FILENAME} &
+      while [ $(pgrep -c openscad) -ge $N ]; do sleep 1; done
+    fi
+  done
+done
+wait
+
--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # FabLabRocker
-A simple servo-based lab rocker with variable frequency and amplitude
+
+A simple servo-based lab rocker with variable frequency and amplitude.
+Designed for DIY PCB fabrication, but can be used for large format photography or chemistry.
+3d-design is customizable, written in OpenSCAD and BOSL2.
+Measure your container and make the parts that fit ideally.
+
+## PCB
+
+PCB is as simple as it can get.
+The project is all about DIY PCB fabrication, so of course it is.
+It is one-sided and basically connects a ready-made modules.
+Power from a cell does through a switch to DC-DC converter that powers Arduino and the servo.
+Two potentiometers are wired to 0 and 5v rails and to analog inputs of Arduino.
+
+### Parts list
+
+TODO: write down particular parts used
+
+- Arduino Nano or Pro Micro
+- 2 linear Potentiometers, about 25k or more
+- DC-DC converter
+- 18650 holder
+- Pin headers
+- Switch
+- Servo
+- 3d-printed parts
+
+## 3d-printed parts
+
+Model parameters are grouped into self-explanatory categories.
+most important are ```bath_width```, ```bath_depth``` which are measurements of container bottom, and ```bath_rounding``` and ```bath_chamfer```, which define corner rounding of the container.
+Of the last two parameters only one can be non-zero.
+Print as is, for the ```top_mech``` part use organic supports.
+
+## Assembly
+
+Assemble the PCB.
+KiCad project is in ```agitator_pcb```.
+Don't forget to set the voltage of DC-DC converter to 5v.
+Customize the mechanical parts and print them.
+The moveable joints are formed by two M4x8 screws.
+For the top assembly use 4 thread inserts in ```top_mech```, for mounting the motor use 2 in ```bottom``` and matching M2x8 screws.
+Or don't. You can use self-tapping screws or glue or whatever.
+PCB is held by the pots.
+
+### Usage
+
+One pot changes the oscillation frequency, the other controls amplitude.
+High amplitudes are not achievable with high frequencies.
+If the servo does not have time to rotate to the goal position it will never achieve the amplitude.
